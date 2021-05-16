568065357eea8859c79d4a77360b4e673be601b5
CrescentRose-orz <841320766@qq.com>
2021-05-16 15:27:41 +0800

    topSort done

--- src/SeuLex/dev/graph.h
+++ src/SeuLex/dev/graph.h
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
+#define to(x) _edge[x].to
+#define next(x) _edge[x].next
 
 using namespace std;
 
 
 class edge{
+public:    
     int next;
     int to;
-public:
     edge(int to,int next){
         this-> next = next;
         this-> to = to;
@@ -16,10 +18,18 @@ public:
     }
 };
 
+/*
+    This graph only support node labeled with 1-n.
+    The memory complexity is O(n + m)
+    n is the largest label number
+    m is the number  of edge
+    for unconsistent label number / string ,use mappingGraph instead
 
+*/
 class graph{
     vector<int> head;
     vector<edge> _edge;
+    int maxN ;
 public :
     graph(){
         //head.push_back(0);
@@ -32,18 +42,44 @@ public :
         }
         edge _tmpEdge(to,head[from]);
         head[from] = _edge.size();
-        _edge.push_back(_tmpEdge);
+        _edge.emplace_back(_tmpEdge);
+        maxN = max(maxN, max(from, to));
         return _edge.size() - 1;
     } 
     void reSize(int size){
         if (head.size() < size)
             head.resize(size);
     }
-    vector<int> topSort(int n){
+    vector<int> topSort(int n, bool &hasCircle){
+        hasCircle = false;
         vector<int> rt;
-
-
-
+        vector<int> inDeg;
+        queue<int> toHandle;
+        inDeg.resize(maxN+1);
+        int e;
+        for (int s = 1; s <= n; ++s){
+            for (e = head[s] ; e ; e =next(e) ){
+                ++inDeg[to(e)];
+            }
+        }
+        for (int s = 1; s <= n; ++s){
+            if (!inDeg[s]){
+                toHandle.push(s);
+            }
+        }
+        while(!toHandle.empty()){
+            int s = toHandle.front();
+            toHandle.pop();
+            rt.emplace_back(s);
+            for (e = head[s] ; e ; e =next(e) ){
+                if(!(--inDeg[to(e)])){
+                    toHandle.push(to(e));
+                }
+            }  
+        }
+        if (rt.size()!=n){
+            hasCircle = true;
+        }
         return rt;
     }
 };
@@ -67,8 +103,8 @@ public :
     void add_edge(T from,T to){
         _graph.add_edge(checkExist(from), checkExist(to))
     }
-    vector<T> topSort(){
-        vector<int> &&tmp = _graph.topSort(tot);
+    vector<T> topSort(bool &hasCircle){
+        vector<int> &&tmp = _graph.topSort(tot,hasCircle);
         vector<T> rt;
         rt.resize(tot);
         for (int i = 0 ; i < rt.size(); ++i){
