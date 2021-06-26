cc6ed345563600117493fd3b4e2cac7b4920d055
CrescentRose-orz <841320766@qq.com>
2021-06-26 23:30:43 +0800

    LR_Node almost finished

--- src/SeuYacc/include/LR_Node.h
+++ src/SeuYacc/include/LR_Node.h
@@ -3,6 +3,8 @@
 #include"header.h"
 #include<vector>
 #include"visualLR.hpp"
+#include"Hash.hpp"
+#include"LR_Producer.hpp"
 using namespace std;
 
 
@@ -15,6 +17,9 @@ private:
     #ifdef VISUAL
     visualLR<int> &fa; 
     #endif
+    unordered_map<int,LR_Producer> producers;
+    eclosureHash coreHash;
+    eclosureHash LR_Node_Hash;
 public:
     int idx;
     #ifdef VISUAL
@@ -22,12 +27,39 @@ public:
     #else
     //LR_Node();
     #endif
-    //添加转移边c到节点target
+    /**
+     * @brief 对项集族做扩展
+     * 
+     */
+    void solveEclosure();
+    /**
+     * @brief 添加一个产生式（若已存在，给已存在的产生式添加向前看符）
+     * 内部提供判重检查
+     * 
+     * @param producer 
+     */
+    void addProducer(LR_Producer producer);
+    /**
+     * @brief 添加转移边c到节点target
+     * 
+     * @param target 转移到的LR节点id
+     * @param c 符号id
+     */
     void addTrans(int target,int c);
-    // 返回读入符号c的转移后状态，若非法返回-1
-    int getTrans(int c){
-        return state.count(c)?state[c]:-1;
-    }
+    /**
+     * @brief 返回读入符号c的转移后状态，若非法返回-1
+     * 
+     * @param c 符号id
+     * @return int 转移后LR节点id
+     */
+    int getTrans(int c);
+    /**
+     * @brief 返回LR项集的中心项哈希\n
+     *   用法：LR_Node n1,n2;
+     *   LALR判同心项集：n1.core()==n2.core()
+     * @return eclosureHash 
+     */
+    eclosureHash core();
 };
 
 
--- src/SeuYacc/src/LR_Node.cpp
+++ src/SeuYacc/src/LR_Node.cpp
@@ -17,3 +17,16 @@ void LR_Node::addTrans(int target,int c){
 int LR_Node::getTrans(int c){
     return state.count(c)?state[c]:-1;
 }
+
+void LR_Node::addProducer(LR_Producer producer){
+    
+}
+
+eclosureHash LR_Node::core(){return coreHash;}
+
+int LR_Node::getTrans(int c){return state.count(c)?state[c]:-1;}
+
+void LR_Node::solveEclosure(){
+
+
+}
