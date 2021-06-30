b48750cd07354a0343159edd4e9463f6bf329419
CrescentRose-orz <841320766@qq.com>
2021-07-01 00:05:34 +0800

    visual LR done

--- src/SeuYacc/include/visualLR.hpp
+++ src/SeuYacc/include/visualLR.hpp
@@ -23,7 +23,7 @@ public :
             trans[i] += (char)i;
         }
         trans['\n']="\\\\n";
-        trans["] = "ε";
+        //trans["] = "ε";
         trans['\\'] ="\\\\";
         trans['\"']="\\\"";
         doubleCircle.clear();
@@ -31,10 +31,36 @@ public :
     }
 
     void printNode(fstream &fout,LR_Node &node){
-        fout<<R"()"
-  "state9" [ style = "filled" penwidth = 1 fillcolor = "white" fontname = "Courier New" shape = "Mrecord" label =<<table border="0" cellborder="0" cellpadding="3" bgcolor="white"><tr><td bgcolor="black" align="center" colspan="2"><font color="white">State #9</font></td></tr><tr><td align="left" port="r2">&#40;2&#41; e -&gt; r &bull;</td><td bgcolor="grey" align="right">$</td></tr></table>> ];
+        fout<<"state"<<node.idx<<R"([ style = "filled" penwidth = 1 fillcolor = "white" fontname = "Courier New" shape = "Mrecord" label =<<table border="0" cellborder="0" cellpadding="3" bgcolor="white"><tr><td bgcolor="black" align="center" colspan="2"><font color="white">State #)"<<node.idx<<"</font></td></tr>)";
+        for (auto iter = node.producers.cbegin();iter != node.producers.cend();++iter){
+            printProducer(fout,iter->second);
+        }
+        fout<<"</table>> ];"<<endl;
+        for (auto iter = node.getAllTransBegin();iter!=node.getAllTransEnd();++iter){
+            fout<<"state"<<node.idx<<"->state"<<iter->second;
+            if (isTerminal(iter->first)){
+                fout<<R"([ penwidth = 1 fontsize = 14 fontcolor = "grey28" label = ")"<<I2S(iter->first)<<" ]"<<endl;
+            } else {
+                fout<<R"([ penwidth = 5 fontsize = 28 fontcolor = "black" label = ")"<<I2S(iter->first)<<" ]"<<endl;
+            }
+        }
     }
 
+    void printProducer(fstream &fout,LR_Producer pro){
+        fout<<R"(<tr><td align="left" port="r")<<pro.producer<<R"(">&#40;")<<pro.producer<<R"(&#41;)";
+        fout<<I2S(getLeft(pro.producer))<<" -&gt ";
+        four (int i = 0; i < getRight(pro.producer).size();++i){
+            if (i == pro.nowPlace){
+                fout<<"&bull;";
+            }
+            fout<<I2S(getRight(pro.producer)[i]);
+            fout<<R"(</td><td bgcolor="grey" align="right">)"
+            for (auto &id:pro.lookAhead){
+                fout<<I2S(id)<<" ";
+            }
+            fout<<R"(</td></tr>)";
+        }
+    }
     void print(fstream &fout,LR &_LR)
     {
         fout<<"digraph finite_state_machine {"<<endl<<"rankdir=LR;"<<endl;
@@ -42,6 +68,8 @@ public :
         for (auto &node:_LR.pool){
             printfNode(fout,node);
         }
+        fout<<"}"<<endl;
+        fout.close();
     }
 };
 
