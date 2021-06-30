0b56da128fcb2dd43f5c6127ce964121104bac23
yggycs <213180808@seu.edu.cn>
2021-06-30 17:34:10 +0800

    更新了求maxLen和maxIdx的代码

--- src/SeuYacc/src/Transform.cpp
+++ src/SeuYacc/src/Transform.cpp
@@ -18,17 +18,13 @@ void transformTerminals(unordered_set<string>& Terminals_Str, unordered_map<stri
 void transformNonterminals(unordered_set<string>& Nonterminals_Str, unordered_map<string, int>& StrToInt, unordered_map<int, string>& IntToStr, int& TNBound, int& NLBound){
     
     int counter = TNBound;
-    producerBaseIdx.resize(TNBound);
-    maxLen = 1;
 	for (auto str : Nonterminals_Str){
 		StrToInt.insert(make_pair(str, counter));
 		IntToStr.insert(make_pair(counter, str));
-        for (;(1<<maxLen) <= str.length(); ++maxIdx);
 		counter++;
 	}
 
 	NLBound = counter;
-    for (maxIdx = 1;(1<<maxIdx) <= NLBound; ++maxIdx);
 }
 
 void transformLeft(unordered_set<string>& Left_Str, unordered_map<string, int>& StrToInt, unordered_map<int, string>& IntToStr, int& NLBound){
@@ -43,10 +39,14 @@ void transformLeft(unordered_set<string>& Left_Str, unordered_map<string, int>& 
 }
 
 void transformTranslationRule(ProducerVecStr& TranslationRule_Str, ProducerVecInt& TranslationRule_Int, unordered_map<string, int>& StrToInt, unordered_map<int, string>& IntToStr){
-	
+	int maxLength = 1;
+
     // 遍历每一个产生式项
     for (auto temp_producer : TranslationRule_Str){
-		
+
+		if(temp_producer.second.size() > maxLength)
+            maxLength = temp_producer.second.size();
+
         vector<int> temp_vector;
 		temp_vector.clear();
 		// 遍历每一个产生式右部项
@@ -56,12 +56,14 @@ void transformTranslationRule(ProducerVecStr& TranslationRule_Str, ProducerVecIn
 		TranslationRule_Int.push_back(make_pair(StrToInt[temp_producer.first], temp_vector));
 	}
 
+/*
     // 产生Producer->Position的映射
 	for (int i = 0; i < TranslationRule_Int.size(); i++){
         vector<int> temp = TranslationRule_Int[i].second;
 		temp.insert(temp.begin(), TranslationRule_Int[i].first);
-		//ProToPos.insert(make_pair(temp, i));
+		ProToPos.insert(make_pair(temp, i));
 	}
+*/
 
     // 产生LHS->Position的映射
     // 利用相同产生式左部的产生式连续存放的规律，只需遍历一遍产生式集合即可
@@ -85,4 +87,16 @@ void transformTranslationRule(ProducerVecStr& TranslationRule_Str, ProducerVecIn
             continue;
         }
     }
+
+    // 求maxIdx，即大于等于产生式下标的最小2的整数次幂
+    maxIdx = 0;
+    int tempNum = 1;
+    for(; tempNum <= NLBound + 1; maxIdx++)
+        tempNum *= 2;
+
+    // 求maxLen，即大于等于右部长度最大值的最小2的整数次幂
+    maxLen = 0;
+    int tempNum2 = 1;
+    for(; tempNum2 <= maxLength + 1; maxLen++)
+        tempNum2 *= 2;
 }
