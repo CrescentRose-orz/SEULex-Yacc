377023faa9e7b4c65cf201818a17981b99a08cf1
yggycs <213180808@seu.edu.cn>
2021-06-30 15:58:14 +0800

    引入函数头文件

--- src/SeuYacc/src/First.cpp
+++ src/SeuYacc/src/First.cpp
@@ -1,5 +1,4 @@
-#include "header.h"
-#include "DataStructure.h"
+#include "First.h"
 
 using namespace std;
 
@@ -17,8 +16,6 @@ using namespace std;
 // Int形式产生式左部->Int形式产生式右部的映射关系
 //extern unordered_map<int, vector<int>> LHSToPos;
 
-// 该函数对单个字符求其first，是一个递归算法
-// 该函数的输入依次为所求字符，返回结果集，以及一个维护的已求过first的非终结符集合
 void getFirst(const int& symbol, unordered_set<int>& resultSet, unordered_set<int>& processedSymbol) {
 
 	// 该符号之前已求过first
@@ -95,8 +92,6 @@ void getFirst(const int& symbol, unordered_set<int>& resultSet, unordered_set<in
 	}
 }
 
-
-// 该函数求取所有符号的first并存入数据结构First中供后续调用
 void getFirst() {
 	unordered_set<int> firstSet, stackSet;
 
