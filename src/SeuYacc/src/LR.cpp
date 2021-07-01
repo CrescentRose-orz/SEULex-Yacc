962491da486b6089f5d98cb72f2905e35798ec84
yggycs <213180808@seu.edu.cn>
2021-07-01 12:50:45 +0800

    构造LALR规约表

--- src/SeuYacc/src/LR.cpp
+++ src/SeuYacc/src/LR.cpp
@@ -1,6 +1,7 @@
 #include"LR.h"
 
-
+// 定义acc
+#define acc StrToInt.size()+1
 
 LR::LR(){
     tail = 0;
@@ -71,4 +72,124 @@ bool flag = 1;
         }
     }
     return  pool.size();
+}
+
+int LR::constructParsingTable(){
+    // 首先生成y.tab.h，其中存储了终结符与int之间的映射
+    ofstream out;
+	out.open("y.tab.h", ios::out);
+	out << "#ifndef Y_TAB_H" << endl;
+	out << "#define Y_TAB_H" << endl;
+
+    out << endl;
+
+    out << "// epsilon" << endl;
+    out << "#difine " << "" <<"epsilon -1" << endl;
+
+    out << endl;
+
+    out << "// terminals" << endl;
+    for (int i = 0; i < TNBound; i++)
+		out << "#difine " << IntToStr[i] <<" "<< i << endl;
+
+    out << endl;
+
+    out << "// nonterminals" << endl;
+    for (int i = TNBound; i < NLBound; i++)
+		out << "#difine " << IntToStr[i] <<" "<< i << endl;
+    
+    out << endl;
+	
+	out << "#endif" << endl;
+	out.close();
+
+    // 其次生成y.tab.cpp，其中存储SeuYacc的输出结果
+    out.open("y.tab.c", ios::out);
+
+    // ----------头文件部分----------
+	out << "#include \"y.tab.h\"" << endl;
+
+    // -----------命名空间-----------
+    out << "using namespace std;" << endl;
+
+    // ---------全局变量部分---------
+    out << endl;
+    int state_num = this->pool.size();
+    int nonterminal_num = NLBound - TNBound;
+    // 生成并输出GOTO表，GOTO表的结构为int[状态个数][非终结符个数]
+    out << "int GOTO[" << state_num << "][" << nonterminal_num << "] = {"<< endl;
+    for(int i = 0; i < state_num; i++){
+        // 存储每一个状态的GOTO子表(一维数组int[非终结符个数])
+        int *temp = new int[nonterminal_num];
+        // 初始化每一项均为-1(出错)
+        for(int j = 0; j < nonterminal_num; j++)
+            temp[j] = -1;
+
+        // 根据项目集簇生成GOTO表
+        for(auto iter = this->pool[i].getAllTransBegin(); iter != this->pool[i].getAllTransEnd(); ++iter){
+            // 确认其为非终结符
+            if(iter->first < NLBound && iter->first >= NLBound)
+                temp[iter->first - NLBound] = iter->second;
+
+
+        // 将temp写入GOTO表
+        out << "\t{";
+        for(int j = 0; j < nonterminal_num - 1; j++)
+            out << temp[j] << ", ";
+        out << temp[nonterminal_num - 1] << "}," << endl;
+
+        delete []temp;
+    }
+
+    out << "};" << endl << endl;
+
+    // 生成并输出ACTION表，ACTION表的结构为int[状态个数][终结符个数]
+    // 约定0为出错，+x代表移进到第x-1个状态，-x代表使用第x-1号产生式规约，+x其中x=StrToInt.size()+1代表acc
+    int terminal_num = TNBound;
+    out << "int ACTION[" << state_num << "][" << terminal_num << "] = {"<< endl;
+    for(int i = 0; i < state_num; i++){
+        // 存储每一个状态的ACTION子表(一维数组int[终结符个数])
+        int *temp = new int[terminal_num];
+        // 初始化每一项均为0(出错)
+        for(int j = 0; j < terminal_num; j++)
+            temp[j] = 0;
+
+        // 根据项目集簇生成ACTION表
+        // 规约项
+        set<LR_Producer> pros = this->pool[i].getAllReduce();
+        for(auto & pro : pros)
+            for(auto & num : pro.lookAhead){
+                // acc情况，即对向前看#符号进行产生式号为0(start->translation_unit)的规约
+                if(pro.producer == 0 && IntToStr[num] == "#")
+                    temp[num] = acc;
+                else
+                    temp[num] = -(pro.producer + 1);
+            }
+        
+        // 移进项
+        for(auto iter = this->pool[i].getAllTransBegin(); iter != this->pool[i].getAllTransEnd(); ++iter)
+            // 确认其为非终结符
+            if(iter->first < NLBound){
+                // 出现移进规约冲突，报错
+                if(temp[iter->first] != 0){
+                    cout << "ERROR: Not A LALR grammar!\n" <<endl;
+                    return 1;
+                }
+                temp[iter->first] = iter->second + 1;
+            }
+
+        // 将temp写入ACTION表
+        out << "\t{";
+        for(int j = 0; j < terminal_num - 1; j++)
+            out << temp[j] << ", ";
+        out << temp[terminal_num - 1] << "}," << endl;
+
+        delete []temp;
+    }
+
+    out << "};" << endl << endl;
+    
+    
+    // 构建成功
+    return 0;
 }
